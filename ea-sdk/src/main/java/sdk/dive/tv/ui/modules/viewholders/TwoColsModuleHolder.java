6aa9e418ce5c266b06136e7417ad95774a372eb7
Emilio Alvarez <emilio.alvarez@dive.tv>
2018-05-08 17:18:32 +0200

    working in vertical list

--- ea-sdk/src/main/java/sdk/dive/tv/ui/modules/viewholders/TwoColsModuleHolder.java
+++ ea-sdk/src/main/java/sdk/dive/tv/ui/modules/viewholders/TwoColsModuleHolder.java
@@ -1,13 +1,17 @@
 package sdk.dive.tv.ui.modules.viewholders;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 
 import com.squareup.picasso.Picasso;
 import com.touchvie.sdk.model.Card;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import sdk.dive.tv.ui.Utils;
+import sdk.dive.tv.ui.data.ModuleStyleData;
 import sdk.dive.tv.ui.listeners.SectionListener;
 import sdk.dive.tv.ui.listeners.TvCardDetailListener;
 import sdk.dive.tv.ui.modules.adapters.TwoColsModuleAdapter;
@@ -21,6 +25,7 @@ public abstract class TwoColsModuleHolder extends VerticalListHolder {
 
     protected TwoColsModuleAdapter adapter;
     protected ArrayList<TwoTextRowData> data;
+    private HashMap<String, ModuleStyleData> genericStyles;
 
     /**
      * Default constructor
@@ -39,6 +44,13 @@ public abstract class TwoColsModuleHolder extends VerticalListHolder {
 //        mContainer.setPadding(0, 0, 0, 0);
         data = new ArrayList<>();
         adapter = new TwoColsModuleAdapter(context, data, tvCardDetailListener);
+        if (tvCardDetailListener != null && tvCardDetailListener.getGenericStyles() != null){
+            genericStyles = tvCardDetailListener.getGenericStyles();
+            mContainer.setBackground(Utils.makeSelector(Color.parseColor(genericStyles.get("selectedColor").getValue()),Color.parseColor("#00000000"), genericStyles.get("backgroundModuleColor").getValue()));
+            btnDown.setBackground(Utils.makeSelector(Color.parseColor(genericStyles.get("selectedColor").getValue()),Color.parseColor(genericStyles.get("unselectedColor").getValue()), genericStyles.get("selectedColor").getValue()));
+            btnUp.setBackground(Utils.makeSelector(Color.parseColor(genericStyles.get("selectedColor").getValue()),Color.parseColor(genericStyles.get("unselectedColor").getValue()), genericStyles.get("selectedColor").getValue()));
+        }
+
         super.setAdapter(adapter, false, null);
     }
 
